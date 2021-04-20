7e40189af990b2cade1893773fab7833e1a661c3
Adeilson Silva <adeilson@gmail.com>
2021-04-20 19:43:04 -0300

    aula 2

---
+++ flutter/lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,48 @@
+import 'package:dev_quiz/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:dev_quiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(height: 24),
+          AwnserWidget(
+            title:
+                "Possibilita a criação de aplicativos compilados nativamente",
+            isRight: false,
+            isSelected: false,
+          ),
+          AwnserWidget(
+            title:
+                "Possibilita a criação de aplicativos compilados nativamente",
+            isRight: true,
+            isSelected: true,
+          ),
+          AwnserWidget(
+            title:
+                "Possibilita a criação de aplicativos compilados nativamente",
+            isRight: false,
+            isSelected: false,
+          ),
+          AwnserWidget(
+            title:
+                "Possibilita a criação de aplicativos compilados nativamente",
+            isRight: false,
+            isSelected: false,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter/lib/home/home_page.dart
+++ flutter/lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:dev_quiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +13,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LeveButtonWidget(
+                label: "Fácil",
+              ),
+              LeveButtonWidget(
+                label: "Médio",
+              ),
+              LeveButtonWidget(
+                label: "Difícil",
+              ),
+              LeveButtonWidget(
+                label: "Perito",
+              ),
+            ],
+          ),
+          SizedBox(height: 24),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+              children: [
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
